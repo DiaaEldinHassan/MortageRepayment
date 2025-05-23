436d23b6cb68703bbe0df1710d3c0c2ff2666a97
DiaaEldinHassan <diaahassan777@gmail.com>
2025-05-23 06:24:04 +0300

    fourth commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import "./left.css";
-import "./right.css";
+import "./Left/left.css";
+import "./Right/right.css";
 
 import App from './App.jsx'
 
